74f43030c2ab636d1beb4f1d927f03161b48e5ea
Nico-Serra <nico.serra000@gmail.com>
2024-07-10 12:10:54 +0200

    add site structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { CategoryComponent } from './category/category.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, JumbotronComponent, CategoryComponent, CoursesComponent, ReviewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
